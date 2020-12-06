7a6ddb8bd9d9367b87b10ccdc6b010eaa12471f3
Linnea Uden <59481798+Linnea-Uden1@users.noreply.github.com>
2020-12-06 12:33:29 +0100

    Add files via upload

---
+++ TeknikUppgift/app.js
@@ -0,0 +1,14 @@
+// function othername() {
+//     var input = document.getElementById("userInput").value;
+//     alert(input);
+
+// }
+
+function othername() {
+    var x = document.getElementById("userInput").value;
+    document.getElementById("output").innerHTML = x;
+    document.getElementById('userInput').value = '';
+}
+
+
+
